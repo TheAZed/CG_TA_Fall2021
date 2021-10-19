65773b51c2416f9f391fe0dbf278f3351d65ffad
Alireza Zohri <zohri@balad.ir>
2021-10-19 16:52:01 +0330

    Add base code

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,5 +1,9 @@
 import opengl.GLContext
+import opengl.GLProgram
 import org.lwjgl.opengl.GL11.*
+import org.lwjgl.opengl.GL15
+import org.lwjgl.opengl.GL20.*
+import org.lwjgl.opengl.GL33.*
 import kotlin.math.abs
 
 
@@ -9,11 +13,50 @@ fun main() {
     context.use()
     context.show()
 
+    val testProgram = GLProgram("first.vert", "first.frag")
+    testProgram.use()
+
+    val data = floatArrayOf(
+        -0.5f, -0.5f, 0.0f,        1.0f, 0.0f, 0.0f,
+        0.5f, -0.5f, 0.0f,        0.0f, 1.0f, 0.0f,
+        0.5f, 0.5f, 0.0f,        0.0f, 0.0f, 1.0f,
+        -0.5f, 0.5f, 0.0f,        1.0f, 1.0f, 0.0f,
+    )
+
+    val indices = intArrayOf(
+        0, 1, 2,
+        3, 0, 1,
+    )
+//    val color = floatArrayOf(1.0f, 0.5f, 0.0f)
+//
+//    testProgram.setVec3Float(color, "uColor")
+    val vao = glGenVertexArrays()
+    glBindVertexArray(vao)
+
+    val vbo = glGenBuffers()
+    glBindBuffer(GL_ARRAY_BUFFER, vbo)
+    glBufferData(GL_ARRAY_BUFFER, data, GL_STATIC_DRAW)
+
+    val ebo = glGenBuffers()
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo)
+    GL15.glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW)
+
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * Float.SIZE_BYTES, 0L)
+    glEnableVertexAttribArray(0)
+    glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * Float.SIZE_BYTES, (3 * Float.SIZE_BYTES).toLong())
+    glEnableVertexAttribArray(1)
+
+    glBindVertexArray(0)
+    glBindVertexArray(vao)
+
     context.drawLoop {
-        val color = (System.currentTimeMillis() / 2 % 1000) / 1000f
-        glClearColor(1.0f, abs(1 - 2 * color), 0.0f, 1.0f)
+//        val color = (System.currentTimeMillis() / 2 % 1000) / 1000f
+//        glClearColor(1.0f, abs(1 - 2 * color), 0.0f, 1.0f)
+
         // Clear the framebuffer
         glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0L)
     }
 
     context.free()
---
+++ src/main/kotlin/opengl/GLProgram.kt
@@ -0,0 +1,57 @@
+package opengl
+
+import org.lwjgl.opengl.GL20
+import org.lwjgl.opengl.GL20.*
+
+
+class GLProgram(vertexName: String, fragmentName: String) {
+    private val programID: Int = glCreateProgram()
+
+    init {
+        val classLoader: ClassLoader = Thread.currentThread().contextClassLoader
+        val vertexStr = classLoader.getResourceAsStream(vertexName)?.reader()?.readText()
+        val fragmentStr = classLoader.getResourceAsStream(fragmentName)?.reader()?.readText()
+
+        val vertexShader = glCreateShader(GL_VERTEX_SHADER)
+        glShaderSource(vertexShader, vertexStr!!)
+        glCompileShader(vertexShader)
+        checkCompileErrors(vertexShader, "SHADER")
+
+        val fragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
+        glShaderSource(fragmentShader, fragmentStr!!)
+        glCompileShader(fragmentShader)
+        checkCompileErrors(fragmentShader, "SHADER")
+
+        glAttachShader(programID, vertexShader)
+        glAttachShader(programID, fragmentShader)
+        glLinkProgram(programID)
+        checkCompileErrors(programID, "PROGRAM")
+
+    }
+
+    fun use() {
+        glUseProgram(programID)
+    }
+
+    fun setVec3Float(data: FloatArray, name: String) {
+        val location = glGetUniformLocation(programID, name)
+        glUniform3fv(location, data)
+    }
+
+    private fun checkCompileErrors(shader: Int, type: String) {
+        val success = IntArray(1)
+        if (type == "PROGRAM") {
+            glGetProgramiv(shader, GL_LINK_STATUS, success)
+            if (success[0] == GL_FALSE) {
+                val info = GL20.glGetProgramInfoLog(shader)
+                System.err.println("Error compiling program\n$info\n------------------------------------------")
+            }
+        } else {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, success)
+            if (success[0] == GL_FALSE) {
+                val info = GL20.glGetShaderInfoLog(shader)
+                System.err.println("Error compiling shader of type $type\n$info\n------------------------------------------")
+            }
+        }
+    }
+}
